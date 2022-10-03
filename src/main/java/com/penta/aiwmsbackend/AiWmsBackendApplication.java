2c04f818269c0690bf8cbcae209cc2cd62802caa
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-10-03 17:15:48 +0630

    initial commit

---
+++ src/main/java/com/penta/aiwmsbackend/AiWmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.penta.aiwmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiWmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/penta/aiwmsbackend/AiWmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penta.aiwmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
