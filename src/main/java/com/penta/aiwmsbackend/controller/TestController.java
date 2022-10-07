59900b3939cb92e39999cee0d9c7dc80bb9552f2
zayarmyooo19 <zayarmyooo1832003@gmail.com>
2022-10-07 12:20:05 +0630

    mail test completed

--- src/main/java/com/penta/aiwmsbackend/controller/TestController.java
+++ src/main/java/com/penta/aiwmsbackend/controller/TestController.java
@@ -15,19 +15,26 @@ import com.penta.aiwmsbackend.exception.custom.DuplicateEmailException;
 import com.penta.aiwmsbackend.exception.handler.UserControllerAdvice;
 
 @Controller
-public class TestController extends UserControllerAdvice  {
+public class TestController extends UserControllerAdvice {
 
     @Autowired
     JavaMailSender mailSender;
-    
+
     @GetMapping("/")
-    public boolean test() throws DuplicateEmailException, UnsupportedEncodingException, MessagingException{
+    public boolean test() throws DuplicateEmailException, UnsupportedEncodingException, MessagingException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-        
-        helper.setFrom("myominhtoo2003@gmail.com" , "You see");
-        helper.setTo("myominhtoo@ucsy.edu.mm");
-        
+
+        helper.setFrom("myominhtoo2003@gmail.com", "You see");
+        // First Method
+        // helper.setTo(new String[] { "myominhtoo@ucsy.edu.mm",
+        // "sunandaraung1211@gmail.com", "kyawswarapr2020@gmail.com",
+        // "kaungmyat97k@gmail.com" });
+
+        // Second Method
+        // helper.setTo(
+        // InternetAddress.parse("myominhtoo@ucsy.edu.mm, sunandaraung1211@gmail.com,
+        // kyawswarapr2020@gmail.com"));
         helper.setSubject("Test");
         helper.setText("<code>const [state,setState] = useState()</code>", true);
         mailSender.send(mimeMessage);
