cacad126fb436b2f389e0c324f52893da6e5b487
zayarmyooo19 <zayarmyooo1832003@gmail.com>
2022-10-07 12:50:38 +0630

    email service added

--- src/main/java/com/penta/aiwmsbackend/model/service/impl/EmailServiceImpl.java
+++ src/main/java/com/penta/aiwmsbackend/model/service/impl/EmailServiceImpl.java
@@ -1,5 +1,57 @@
 package com.penta.aiwmsbackend.model.service.impl;
 
-public class EmailServiceImpl {
-    
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import com.penta.aiwmsbackend.model.service.EmailService;
+
+public class EmailServiceImpl implements EmailService {
+    JavaMailSender mailSender;
+
+    @Autowired
+    public EmailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public boolean verifyEmail(
+            String fromMail,
+            String mailHeader,
+            String sentTo,
+            String subject,
+            String mailContent) throws UnsupportedEncodingException, MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+
+        helper.setFrom(fromMail, mailHeader);
+        helper.setTo(sentTo);
+        helper.setSubject(subject);
+        helper.setText(mailContent, true);
+        mailSender.send(mimeMessage);
+        return false;
+    }
+
+    @Override
+    public boolean InviteMember(String fromMail,
+            String mailHeader,
+            String[] sentTo,
+            String subject,
+            String mailContent) throws UnsupportedEncodingException, MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+
+        helper.setFrom(fromMail, mailHeader);
+        helper.setTo(sentTo);
+        helper.setSubject(subject);
+        helper.setText(mailContent, true);
+        mailSender.send(mimeMessage);
+        return false;
+    }
+
 }
