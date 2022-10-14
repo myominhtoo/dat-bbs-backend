5380461ae63bc73404ef23feed10f1e62e285aa2
BForg <sithulwin2627@gmail.com>
2022-10-14 12:12:59 -0700

    exception task-card && invalid-board-id

---
+++ src/main/java/com/penta/aiwmsbackend/exception/custom/InvalidBoardIdException.java
@@ -0,0 +1,9 @@
+package com.penta.aiwmsbackend.exception.custom;
+
+public class InvalidBoardIdException extends Exception {
+
+    public InvalidBoardIdException(String message) {
+        super(message);
+    }
+
+}
