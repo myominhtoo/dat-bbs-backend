1a912a6d93366751075704d8197f0f9eb4d3d67b
zayarmyooo19 <zayarmyooo1832003@gmail.com>
2022-10-15 12:47:32 +0630

    Create TaskCard added

--- src/main/java/com/penta/aiwmsbackend/exception/custom/DublicateTaskCardNameException.java
+++
@@ -1,9 +0,0 @@
-package com.penta.aiwmsbackend.exception.custom;
-
-public class DublicateTaskCardNameException extends Exception {
-
-    public DublicateTaskCardNameException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/penta/aiwmsbackend/model/service/TaskCardService.java
+++ src/main/java/com/penta/aiwmsbackend/model/service/TaskCardService.java
@@ -1,18 +1,46 @@
 package com.penta.aiwmsbackend.model.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Service;
 
+import com.penta.aiwmsbackend.exception.custom.DuplicateTaskCardNameException;
+import com.penta.aiwmsbackend.exception.custom.InvalidBoardIdException;
+import com.penta.aiwmsbackend.model.entity.Board;
+import com.penta.aiwmsbackend.model.entity.TaskCard;
+import com.penta.aiwmsbackend.model.repo.BoardRepo;
 import com.penta.aiwmsbackend.model.repo.TaskCardRepo;
 
 @Service("cardService")
 public class TaskCardService {
-    
+
     private TaskCardRepo taskCardRepo;
 
+    private BoardRepo boardRepo;
+
     @Autowired
-    public TaskCardService( TaskCardRepo taskCardRepo ){
+    public TaskCardService(TaskCardRepo taskCardRepo, BoardRepo boardRepo) {
         this.taskCardRepo = taskCardRepo;
+        this.boardRepo = boardRepo;
     }
 
+    public boolean CreateTask(TaskCard task) throws InvalidBoardIdException, DuplicateTaskCardNameException {
+        Optional<Board> boardStatus = boardRepo.findById(task.getBoard().getId());
+        if (boardStatus.isEmpty()) {
+            throw new InvalidBoardIdException("Invalid Board !!");
+        } else {
+            List<TaskCard> taskCardList = boardStatus.get().getTaskCards();
+            for (TaskCard taskCardName : taskCardList) {
+                if (taskCardName.getTaskName().equalsIgnoreCase(task.getTaskName())) {
+                    throw new DuplicateTaskCardNameException("Duplicate TaskCardName !!");
+                }
+            }
+            taskCardRepo.save(task);
+            return true;
+        }
+
+    }
 }
