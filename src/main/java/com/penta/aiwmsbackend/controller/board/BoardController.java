e95ba3501dfb6eaaedc3fb8d9218aaf477b1534c
zayarmyooo19 <zayarmyooo1832003@gmail.com>
2022-10-15 19:55:23 +0630

    test

--- src/main/java/com/penta/aiwmsbackend/controller/board/BoardController.java
+++ src/main/java/com/penta/aiwmsbackend/controller/board/BoardController.java
@@ -2,6 +2,7 @@ package com.penta.aiwmsbackend.controller.board;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Date;
+import java.util.List;
 
 import javax.mail.MessagingException;
 
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,7 @@ import com.penta.aiwmsbackend.exception.custom.JoinPermissionException;
 import com.penta.aiwmsbackend.exception.handler.BoardControllerAdvice;
 import com.penta.aiwmsbackend.model.bean.HttpResponse;
 import com.penta.aiwmsbackend.model.entity.Board;
+import com.penta.aiwmsbackend.model.entity.TaskCard;
 import com.penta.aiwmsbackend.model.service.BoardService;
 
 /*
@@ -39,17 +42,17 @@ public class BoardController extends BoardControllerAdvice {
     }
 
     @PostMapping(value = "/create-board")
-    public ResponseEntity<HttpResponse> createBoard(@RequestBody Board board) throws UnsupportedEncodingException, MessagingException, CreatePermissionException {
-        this.boardService.createBoard( board );
+    public ResponseEntity<HttpResponse> createBoard(@RequestBody Board board)
+            throws UnsupportedEncodingException, MessagingException, CreatePermissionException {
+        this.boardService.createBoard(board);
         HttpResponse httpResponse = new HttpResponse(
-            new Date(),
-            HttpStatus.OK,
-            HttpStatus.OK.value(),
-            "Successfully Created!",
-            "Ok",
-            true
-        );
-        return new ResponseEntity<>(httpResponse,httpResponse.getHttpStatus());
+                new Date(),
+                HttpStatus.OK,
+                HttpStatus.OK.value(),
+                "Successfully Created!",
+                "Ok",
+                true);
+        return new ResponseEntity<>(httpResponse, httpResponse.getHttpStatus());
     }
 
     // join-board?email=...&code=123
