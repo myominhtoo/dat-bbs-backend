e048d346bd749d2416d98a3eecc022a3e28dab78
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-10-17 21:29:48 +0630

    fixed

--- src/main/java/com/penta/aiwmsbackend/model/bean/CustomUserDetails.java
+++ src/main/java/com/penta/aiwmsbackend/model/bean/CustomUserDetails.java
@@ -29,7 +29,7 @@ public class CustomUserDetails implements UserDetails{
 
     @Override
     public String getUsername() {
-       return this.user.getUsername();
+       return this.user.getEmail();
     }
 
     @Override
