f5963e9d2b81e8d9e302c57b449aae7b624cb875
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-10-20 19:53:30 +0630

    added cross origin in task controller

--- src/main/java/com/penta/aiwmsbackend/controller/card/TaskCardController.java
+++ src/main/java/com/penta/aiwmsbackend/controller/card/TaskCardController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import com.penta.aiwmsbackend.model.service.TaskCardService;
 
 @RestController
 @RequestMapping(value = "/api")
+@CrossOrigin( originPatterns = "*")
 public class TaskCardController {
 
     private TaskCardService taskCardService;
