c9631655d1f5134622d2b413383565ee07445138
SuNandarAung1211 <sunandaraung1211@gmail.com>
2022-10-22 20:08:22 +0630

    added findactivitybytaskid method

--- src/main/java/com/penta/aiwmsbackend/controller/activity/ActivityController.java
+++ src/main/java/com/penta/aiwmsbackend/controller/activity/ActivityController.java
@@ -1,12 +1,15 @@
 package com.penta.aiwmsbackend.controller.activity;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +19,9 @@ import com.penta.aiwmsbackend.exception.custom.DuplicateActivityNameException;
 import com.penta.aiwmsbackend.exception.custom.InvalidTaskCardIdException;
 import com.penta.aiwmsbackend.model.bean.HttpResponse;
 import com.penta.aiwmsbackend.model.entity.Activity;
+import com.penta.aiwmsbackend.model.entity.TaskCard;
 import com.penta.aiwmsbackend.model.service.ActivityService;
+import com.penta.aiwmsbackend.model.service.TaskCardService;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -44,6 +49,10 @@ public class ActivityController {
             );
             return new ResponseEntity<HttpResponse<Activity>>(httpResponse , httpResponse.getHttpStatus());
         }
-   
-}
 
+        @GetMapping(value = "/task-card/{taskCardId}/activities")
+        public ResponseEntity<List<Activity>> showActivity(@PathVariable("taskCardId") Integer taskCardId)  {
+            List<Activity> showActivities = activityService.showActivities(taskCardId);
+           return ResponseEntity.ok().body(showActivities);
+    }   
+}
