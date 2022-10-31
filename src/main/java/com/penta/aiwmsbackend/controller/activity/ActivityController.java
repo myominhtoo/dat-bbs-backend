4d2fd2618b9073fc4582a93cd01999f3617de639
zayarmyooo19 <zayarmyooo1832003@gmail.com>
2022-10-31 15:22:59 +0630

    fixed update activity

--- src/main/java/com/penta/aiwmsbackend/controller/activity/ActivityController.java
+++ src/main/java/com/penta/aiwmsbackend/controller/activity/ActivityController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.penta.aiwmsbackend.exception.custom.DuplicateActivityNameException;
+import com.penta.aiwmsbackend.exception.custom.InvalidActivityIdException;
 import com.penta.aiwmsbackend.exception.custom.InvalidTaskCardIdException;
 import com.penta.aiwmsbackend.model.bean.HttpResponse;
 import com.penta.aiwmsbackend.model.entity.Activity;
@@ -24,56 +25,56 @@ import com.penta.aiwmsbackend.model.service.ActivityService;
 
 @RestController
 @RequestMapping(value = "/api")
-@CrossOrigin( originPatterns = "*")
+@CrossOrigin(originPatterns = "*")
 public class ActivityController {
 
     private ActivityService activityService;
 
     @Autowired
-    public ActivityController( ActivityService activityService ) {
+    public ActivityController(ActivityService activityService) {
         this.activityService = activityService;
     }
+
     @PostMapping(value = "/create-activity")
-        public ResponseEntity<HttpResponse<Activity>> createActivity (@RequestBody Activity activity)
-        throws InvalidTaskCardIdException , DuplicateActivityNameException {
-            Activity createActivityStatus = activityService.createActivity(activity);
-            HttpResponse<Activity> httpResponse = new HttpResponse<>(
+    public ResponseEntity<HttpResponse<Activity>> createActivity(@RequestBody Activity activity)
+            throws InvalidTaskCardIdException, DuplicateActivityNameException {
+        Activity createActivityStatus = activityService.createActivity(activity);
+        HttpResponse<Activity> httpResponse = new HttpResponse<>(
                 LocalDate.now(),
-                createActivityStatus != null ? HttpStatus.OK : HttpStatus.BAD_REQUEST ,
+                createActivityStatus != null ? HttpStatus.OK : HttpStatus.BAD_REQUEST,
                 createActivityStatus != null ? HttpStatus.OK.value() : HttpStatus.BAD_REQUEST.value(),
                 createActivityStatus != null ? "Successfully Added!" : "Failed to add!",
                 createActivityStatus != null ? "Ok" : "Error Occured!",
-                createActivityStatus != null  ,
-                createActivityStatus
-            );
-            return new ResponseEntity<HttpResponse<Activity>>(httpResponse , httpResponse.getHttpStatus());
-        } 
+                createActivityStatus != null,
+                createActivityStatus);
+        return new ResponseEntity<HttpResponse<Activity>>(httpResponse, httpResponse.getHttpStatus());
+    }
 
-        @GetMapping(value = "/task-card/{taskCardId}/activities")
-        public ResponseEntity<List<Activity>> showActivity(@PathVariable("taskCardId") Integer taskCardId)  {
-            List<Activity> showActivities = activityService.showActivities(taskCardId);
-           return ResponseEntity.ok().body(showActivities);
-        }   
+    @GetMapping(value = "/task-card/{taskCardId}/activities")
+    public ResponseEntity<List<Activity>> showActivity(@PathVariable("taskCardId") Integer taskCardId) {
+        List<Activity> showActivities = activityService.showActivities(taskCardId);
+        return ResponseEntity.ok().body(showActivities);
+    }
 
-        @GetMapping(value = "/task-card/{taskCardId}/activities/{activityId}")
-        public Activity showActivity(@PathVariable ("taskCardId") Integer taskCardId ,@PathVariable ("activityId") Integer activityId)  {
-          return this.activityService.findByActivityId(activityId);
-        }   
-        
-        @PutMapping(value = "/boards/{boardId}/update-activity")
-        public ResponseEntity<HttpResponse<Activity>> UpdateActivity ( @RequestBody Activity activity )
-                        throws InvalidTaskCardIdException, DuplicateActivityNameException {
-             Activity updateStatus = activityService.updateActivity(activity);
-             HttpResponse <Activity> httpResponse = new HttpResponse<>(
-                    LocalDate.now(),
-                    updateStatus !=null ? HttpStatus.OK : HttpStatus.BAD_REQUEST,
-                    updateStatus !=null ? HttpStatus.OK.value() : HttpStatus.BAD_REQUEST.value(),
-                    updateStatus !=null ? "Successfully Updated!" : "Failed to update!",
-                    updateStatus !=null ? "OK!" : "Error Occured!",
-                    updateStatus !=null ,
-                    updateStatus
-                );
-            return new ResponseEntity<HttpResponse<Activity>>(httpResponse, httpResponse.getHttpStatus());
-        }
-                     
+    @GetMapping(value = "/task-card/{taskCardId}/activities/{activityId}")
+    public Activity showActivity(@PathVariable("taskCardId") Integer taskCardId,
+            @PathVariable("activityId") Integer activityId) {
+        return this.activityService.findByActivityId(activityId);
+    }
+
+    @PutMapping(value = "/update-activity")
+    public ResponseEntity<HttpResponse<Activity>> UpdateActivity(@RequestBody Activity activity)
+            throws InvalidTaskCardIdException, DuplicateActivityNameException, InvalidActivityIdException {
+        Activity updateStatus = activityService.updateActivity(activity);
+        HttpResponse<Activity> httpResponse = new HttpResponse<>(
+                LocalDate.now(),
+                updateStatus != null ? HttpStatus.OK : HttpStatus.BAD_REQUEST,
+                updateStatus != null ? HttpStatus.OK.value() : HttpStatus.BAD_REQUEST.value(),
+                updateStatus != null ? "Successfully Updated!" : "Failed to update!",
+                updateStatus != null ? "OK!" : "Error Occured!",
+                updateStatus != null,
+                updateStatus);
+        return new ResponseEntity<HttpResponse<Activity>>(httpResponse, httpResponse.getHttpStatus());
+    }
+
 }
--- src/main/java/com/penta/aiwmsbackend/model/service/ActivityService.java
+++ src/main/java/com/penta/aiwmsbackend/model/service/ActivityService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.penta.aiwmsbackend.exception.custom.DuplicateActivityNameException;
+import com.penta.aiwmsbackend.exception.custom.InvalidActivityIdException;
 import com.penta.aiwmsbackend.exception.custom.InvalidTaskCardIdException;
 import com.penta.aiwmsbackend.model.entity.Activity;
 import com.penta.aiwmsbackend.model.entity.TaskCard;
@@ -20,64 +21,71 @@ public class ActivityService {
     private TaskCardRepo taskCardRepo;
 
     @Autowired
-    public ActivityService ( ActivityRepo activityRepo , TaskCardRepo taskCardRepo ){
+    public ActivityService(ActivityRepo activityRepo, TaskCardRepo taskCardRepo) {
         this.activityRepo = activityRepo;
         this.taskCardRepo = taskCardRepo;
     }
 
-    public Activity createActivity ( Activity activity ) throws InvalidTaskCardIdException, DuplicateActivityNameException{
+    public Activity createActivity(Activity activity)
+            throws InvalidTaskCardIdException, DuplicateActivityNameException {
         activity.setStatus(false);
         activity.setStartedDate(LocalDateTime.now());
         activity.setEndedDate(LocalDateTime.now());
         activity.setDeleteStatus(false);
-        Optional <TaskCard> taskCardStatus = taskCardRepo.findById(activity.getTaskCard().getId());
-        if ( taskCardStatus.isEmpty()){
+        Optional<TaskCard> taskCardStatus = taskCardRepo.findById(activity.getTaskCard().getId());
+        if (taskCardStatus.isEmpty()) {
             throw new InvalidTaskCardIdException("Invalid TaskCard!");
-        }else {
-                 List<Activity> activityList = this.activityRepo.findActivityByTaskCardId(activity.getTaskCard().getId());
-                    for (Activity activityName : activityList) {
-                        if (activityName.getActivityName().equalsIgnoreCase(activity.getActivityName())){
-                            throw new DuplicateActivityNameException("Duplicate Activity Name!");
-                        }
-                    }
-        
+        } else {
+            List<Activity> activityList = this.activityRepo.findActivityByTaskCardId(activity.getTaskCard().getId());
+            for (Activity activityName : activityList) {
+                if (activityName.getActivityName().equalsIgnoreCase(activity.getActivityName())) {
+                    throw new DuplicateActivityNameException("Duplicate Activity Name!");
+                }
+            }
+
             return this.activityRepo.save(activity);
         }
     }
-    
-    public List<Activity> showActivities ( int taskCardId ) {
+
+    public List<Activity> showActivities(int taskCardId) {
         List<Activity> activiyList = activityRepo.findActivityByTaskCardId(taskCardId);
         return activiyList;
-    } 
-    
-    public Activity findByActivityId ( Integer activityId ){
-        Optional<Activity> activity=this.activityRepo.findById(activityId);
-        if (activity.isPresent()){
+    }
+
+    public Activity findByActivityId(Integer activityId) {
+        Optional<Activity> activity = this.activityRepo.findById(activityId);
+        if (activity.isPresent()) {
             return activity.get();
         }
         return null;
     }
 
-    public Activity updateActivity( Activity activity) throws InvalidTaskCardIdException, DuplicateActivityNameException{
-       
-        Optional <TaskCard> taskCardStatus = taskCardRepo.findById(activity.getTaskCard().getId());
-        if ( taskCardStatus.isEmpty()){
+    public Activity updateActivity(Activity activity)
+            throws InvalidTaskCardIdException, DuplicateActivityNameException, InvalidActivityIdException {
+
+        Optional<TaskCard> taskCardStatus = taskCardRepo.findById(activity.getTaskCard().getId());
+        Activity savedActivity = this.activityRepo.findById(activity.getId())
+                .orElseThrow(() -> new InvalidActivityIdException("Invalid activity"));
+
+        if (taskCardStatus.isEmpty()) {
             throw new InvalidTaskCardIdException("Invalid TaskCard!");
-        }else {
-                 List<Activity> activityList = this.activityRepo.findActivityByTaskCardId(activity.getTaskCard().getId());
-                    for (Activity activityName : activityList) {
-                        if (activityName.getActivityName().equalsIgnoreCase(activity.getActivityName()) && 
-                        activityName.getId()!= activity.getId()){
-                            throw new DuplicateActivityNameException("Duplicate Activity Name!");
-                        }
-                    }
-            // activity.setStatus(activity.isStatus()==null ? false : activity.isStatus());
-            activity.setStartedDate(activity.getStartedDate()==null ? LocalDateTime.now() : activity.getStartedDate());
-            activity.setEndedDate(activity.getEndedDate()==null ? LocalDateTime.now() : activity.getEndedDate());
-            
-            return this.activityRepo.save(activity);
+        } else {
+            List<Activity> activityList = this.activityRepo.findActivityByTaskCardId(activity.getTaskCard().getId());
+            for (Activity activityName : activityList) {
+                if (activityName.getId() != activity.getId()
+                        && activityName.getActivityName().equalsIgnoreCase(activity.getActivityName())) {
+                    throw new DuplicateActivityNameException("Duplicate Activity Name!");
+                }
+            }
+
+            savedActivity.setActivityName(activity.getActivityName());
+            savedActivity.setDeleteStatus(activity.isDeleteStatus());
+            savedActivity.setStatus(activity.isStatus());
+            savedActivity.setStartedDate(activity.getStartedDate());
+            savedActivity.setEndedDate(activity.getEndedDate());
+
+            return this.activityRepo.save(savedActivity);
         }
     }
-    
-    
+
 }
