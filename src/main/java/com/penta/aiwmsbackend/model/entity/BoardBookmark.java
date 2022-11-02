42120a2f34cb083360e2abbbb1174f91d2889ef4
kyawswarapr <kyawswarapr2020@gamil.com>
2022-11-02 09:31:47 +0630

    added bookmark backend

--- src/main/java/com/penta/aiwmsbackend/model/entity/BoardBookmark.java
+++ src/main/java/com/penta/aiwmsbackend/model/entity/BoardBookmark.java
@@ -1,6 +1,5 @@
 package com.penta.aiwmsbackend.model.entity;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
--- src/main/java/com/penta/aiwmsbackend/model/service/BoardBookmarkService.java
+++ src/main/java/com/penta/aiwmsbackend/model/service/BoardBookmarkService.java
@@ -1,12 +1,56 @@
 package com.penta.aiwmsbackend.model.service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.penta.aiwmsbackend.model.entity.BoardBookmark;
 import com.penta.aiwmsbackend.model.repo.BoardBookmarkRepo;
 
 @Service
 public class BoardBookmarkService {
-    @Autowired
+
     private BoardBookmarkRepo boardBookmarkRepo;
+
+    @Autowired
+    public BoardBookmarkService(BoardBookmarkRepo boardBookmarkRepo) {
+        this.boardBookmarkRepo = boardBookmarkRepo;
+    }
+
+    public BoardBookmark toggleBoardBookmark(BoardBookmark boardBookmark) {
+        Optional<BoardBookmark> optionalBookMark = this.boardBookmarkRepo
+                .findByUserIdAndBoardId(boardBookmark.getUser().getId(), boardBookmark.getBoard().getId());
+
+        if (optionalBookMark.isEmpty()) {
+            boardBookmark.setCreatedDate(LocalDateTime.now());
+            return this.boardBookmarkRepo.save(boardBookmark);
+        } else {
+            try {
+                this.boardBookmarkRepo.deleteById(boardBookmark.getId());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return boardBookmark;
+    }
+
+    // public BoardBookmark save(BoardBookmark boardBookmark) {
+    // return this.boardBookmarkRepo.save(boardBookmark);
+    // }
+
+    // public List<BoardBookmark> findByUserId(Integer userId) {
+    // return this.boardBookmarkRepo.findByUserId(userId);
+    // }
+
+    // public BoardBookmark findUserByIdAndBoardId(Integer userId, Integer boardId)
+    // {
+    // Optional<BoardBookmark> optionalBoardBookmark =
+    // this.boardBookmarkRepo.findUserByUserIdAndBoardId(userId,
+    // boardId);
+    // return optionalBoardBookmark.isPresent() ? optionalBoardBookmark.get() :
+    // null;
+    // }
+
 }
