5e744eb924b70dfe4f8d0bf1d45fe7cebf3c0596
SuNandarAung1211 <sunandaraung1211@gmail.com>
2022-11-08 21:17:45 +0630

    added bookmask controller & boardMessage service and controller

--- src/main/java/com/penta/aiwmsbackend/controller/chat/BoardChatController.java
+++ src/main/java/com/penta/aiwmsbackend/controller/chat/BoardChatController.java
@@ -1,17 +1,32 @@
 package com.penta.aiwmsbackend.controller.chat;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.penta.aiwmsbackend.model.entity.BoardMessage;
+import com.penta.aiwmsbackend.model.service.BoardMessageService;
 
 @Controller
 public class BoardChatController {
     
     private SimpMessagingTemplate simpMessagingTemplate;
+    private BoardMessageService boardMessageService;
 
     @Autowired
-    public BoardChatController( SimpMessagingTemplate simpMessagingTemplate  ){
+    public BoardChatController( SimpMessagingTemplate simpMessagingTemplate ,BoardMessageService boardMessageService ){
         this.simpMessagingTemplate = simpMessagingTemplate;
+        this.boardMessageService =boardMessageService;
     }
 
+    @GetMapping("boards/{boardId}/boardMessages")
+    public ResponseEntity<List<BoardMessage>> getBoardMessages ( @PathVariable("boardId") Integer id ){
+        List<BoardMessage> getBoardMessages = boardMessageService.getBoardMessageByBoardId(id);
+        return ResponseEntity.ok().body(getBoardMessages);
+    }
 }
--- src/main/java/com/penta/aiwmsbackend/model/repo/BoardMessageRepo.java
+++ src/main/java/com/penta/aiwmsbackend/model/repo/BoardMessageRepo.java
@@ -1,11 +1,16 @@
 package com.penta.aiwmsbackend.model.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.penta.aiwmsbackend.model.entity.BoardMessage;
 
 @Repository
 public interface BoardMessageRepo extends JpaRepository<BoardMessage, Integer> {
-
+ 
+      @Query( value = "SELECT * FROM board_messages WHERE board_id = ?1 ", nativeQuery = true )
+      List <BoardMessage> findBoardMessageByBoardId ( Integer boardId );
 }
