80c8a378c8640ff7cd5b70413b7319f1d279827d
SuNandarAung1211 <sunandaraung1211@gmail.com>
2022-11-08 11:25:49 +0630

    added GetNotiByUserId

--- src/main/java/com/penta/aiwmsbackend/controller/noti/NotificationController.java
+++ src/main/java/com/penta/aiwmsbackend/controller/noti/NotificationController.java
@@ -1,12 +1,33 @@
 package com.penta.aiwmsbackend.controller.noti;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.penta.aiwmsbackend.model.entity.Notification;
+import com.penta.aiwmsbackend.model.service.NotificationService;
 
 @RestController
 @RequestMapping(value = "/api")
 @CrossOrigin( originPatterns = "*")
 public class NotificationController {
-    
+    private NotificationService notificationService;
+
+    @Autowired
+    public NotificationController(NotificationService notificationService){
+        this.notificationService=notificationService;
+    }
+
+    @GetMapping("users/{userId}/notifications")
+    public ResponseEntity<List<Notification>> showNotifications (@PathVariable("userId") Integer userId){
+        List<Notification> showNotifications = notificationService.getNotificationsWithUserId(userId);
+        return ResponseEntity.ok().body(showNotifications);
+    }
 }
