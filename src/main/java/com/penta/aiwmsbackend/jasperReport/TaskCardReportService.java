c6ac4ff1c7bde05f29a8c107dde0d2eebab2e9b9
SuNandarAung1211 <sunandaraung1211@gmail.com>
2022-11-10 23:14:23 +0630

    Jasper Report for boards and taskcards

--- src/main/java/com/penta/aiwmsbackend/jasperReport/TaskCardReportService.java
+++ src/main/java/com/penta/aiwmsbackend/jasperReport/TaskCardReportService.java
@@ -2,22 +2,18 @@ package com.penta.aiwmsbackend.jasperReport;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.stream.util.StreamReaderDelegate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
-import com.penta.aiwmsbackend.model.entity.Board;
-import com.penta.aiwmsbackend.model.repo.BoardRepo;
-
-import net.bytebuddy.utility.nullability.NeverNull.ByDefault;
+import com.penta.aiwmsbackend.model.entity.TaskCard;
+import com.penta.aiwmsbackend.model.service.TaskCardService;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -30,40 +26,49 @@ import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
-
 @Service
-public class BoardReport {
-    
+public class TaskCardReportService {
+
     @Autowired
-    private BoardRepo boardRepo;
+    private TaskCardService taskCardService;
 
-    public String exportReport(String reportFormat) throws JRException, IOException {
-        List<Board> board = new ArrayList<Board>();
-        board = (List<Board>) boardRepo.findBoardsById();
+    public String exportTaskReport(String reportFormat, HttpServletResponse response ) throws JRException, IOException {
+
+        String pathName = System.getProperty("java.class.path").split(";")[0].replace("target\\classes", "")
+                + "src\\main\\resources\\report\\";
+
+        // List<TaskCard> reportTaskCards = taskCardService.reportTaskCards(boardId);
+        List<TaskCard> reportTaskCards = taskCardService.reportTaskCards();
+
         String path = "D:\\Penta\\JasperReport";
-        File file = ResourceUtils.getFile("classpath:board.jrxml");
+        File file = ResourceUtils.getFile(pathName + "taskcardReport.jrxml");
         JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(board);
-        Map<String, Object> parameters = new HashMap<>();
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(reportTaskCards);
 
+        Map<String, Object> parameters = new HashMap<>();
         parameters.put("createdBy", "Admin");
+
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\taskCard.html");
+        }
 
         if (reportFormat.equalsIgnoreCase("pdf")) {
-            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\board.pdf");
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\taskCard.pdf");
         }
+
         if (reportFormat.equalsIgnoreCase("excel")) {
             JRXlsxExporter exporter = new JRXlsxExporter();
             exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(path + "\\board.xlsx"));
 
-            SimpleXlsxReportConfiguration config = new SimpleXlsxReportConfiguration();
-            config.setOnePagePerSheet(true);
-            config.setDetectCellType(true);
-            exporter.setConfiguration(config);
+            SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
+            configuration.setOnePagePerSheet(true);
+            configuration.setDetectCellType(true);
+            exporter.setConfiguration(configuration);
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(path + "\\taskCard.xlsx"));
             exporter.exportReport();
         }
-        return "report generated in path " + path;
+         return "report generated in path " + path;
     }
-
+    
 }
