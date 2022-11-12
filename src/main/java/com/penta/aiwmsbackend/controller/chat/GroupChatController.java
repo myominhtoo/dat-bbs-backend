1f78cf9df3657cdf37feb1c8d93357dffdb73236
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-11-12 23:26:20 +0630

    fixed conflict

--- src/main/java/com/penta/aiwmsbackend/controller/chat/GroupChatController.java
+++ src/main/java/com/penta/aiwmsbackend/controller/chat/GroupChatController.java
@@ -1,11 +1,6 @@
 package com.penta.aiwmsbackend.controller.chat;
 
 import java.time.LocalDateTime;
-<<<<<<< HEAD
-import java.time.LocalTime;
-import java.util.List;
-=======
->>>>>>> ee519c9305349d2d46cfb830e6b6e75a560893ea
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -14,12 +9,6 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-=======
->>>>>>> ee519c9305349d2d46cfb830e6b6e75a560893ea
 import com.penta.aiwmsbackend.model.service.BoardChatService;
 import com.penta.aiwmsbackend.model.entity.BoardMessage;
 
