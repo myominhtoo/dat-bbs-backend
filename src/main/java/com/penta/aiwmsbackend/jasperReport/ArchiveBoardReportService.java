8646235ef819b8c9bdb0e4c782b7986b5599d731
zayarmyooo19 <zayarmyooo1832003@gmail.com>
2022-11-20 19:29:01 +0630

    bar ma ma thi error ma tat aung tin lite tr

---
+++ src/main/java/com/penta/aiwmsbackend/jasperReport/ArchiveBoardReportService.java
@@ -0,0 +1,85 @@
+package com.penta.aiwmsbackend.jasperReport;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import com.penta.aiwmsbackend.model.entity.Board;
+import com.penta.aiwmsbackend.model.service.BoardService;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
+
+@Service
+public class ArchiveBoardReportService {
+
+    @Autowired
+    private BoardService boardService;
+
+    private List<Board> blist;
+
+    public String archiveBoardReport(String format) throws JRException, IOException {
+
+        String filePath = System.getProperty("java.class.path").split(";")[0].replace("target\\classes", "")
+                + "src\\main\\resources\\static\\Exported-Reports";
+
+        String path = System.getProperty("java.class.path").split(";")[0].replace("target\\classes", "")
+                + "src\\main\\resources\\exportedReport";
+
+        File file = ResourceUtils.getFile(filePath + "board.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(this.blist);
+        Map<String, Object> parameters = new HashMap<>();
+
+        parameters.put("createdBy", "Admin");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        if (format.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint,
+                    path + "\\archive-boards" + LocalDate.now() + " " + LocalDateTime.now().getHour() + " hrs "
+                            + LocalDateTime.now().getMinute() + " minutes " + ".html");
+        }
+        if (format.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint,
+                    path + "\\archive-boards" + LocalDate.now() + " " + LocalDateTime.now().getHour() + " hrs "
+                            + LocalDateTime.now().getMinute() + " minutes " + ".pdf");
+        }
+        if (format.equalsIgnoreCase("excel")) {
+            JRXlsxExporter exporter = new JRXlsxExporter();
+            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(
+                    path + "\\archive-boards" + LocalDate.now() + " " + LocalDateTime.now().getHour() + " hrs "
+                            + LocalDateTime.now().getMinute() + " minutes " + ".xlsx"));
+
+            SimpleXlsxReportConfiguration config = new SimpleXlsxReportConfiguration();
+            config.setOnePagePerSheet(true);
+            config.setDetectCellType(true);
+            exporter.setConfiguration(config);
+            exporter.exportReport();
+        }
+        return "report generated in path " + path;
+
+    }
+
+    public void reportBoardList(Integer id) {
+        this.blist = this.boardService.showdeletedBoards(id);
+    }
+
+}
---
+++ src/main/java/com/penta/aiwmsbackend/jasperReport/archiveTasksReportService.java
@@ -0,0 +1,86 @@
+package com.penta.aiwmsbackend.jasperReport;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import com.penta.aiwmsbackend.model.entity.TaskCard;
+import com.penta.aiwmsbackend.model.service.TaskCardService;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
+
+@Service
+public class archiveTasksReportService {
+    @Autowired
+    private TaskCardService taskCardService;
+    private List<TaskCard> tasks;
+
+    public String exportTaskReport(String reportFormat) throws JRException, IOException {
+
+        String pathName = System.getProperty("java.class.path").split(";")[0].replace("target\\classes", "")
+                         .replace("target\\test-classes", "") + "src\\main\\resources\\report\\";
+
+        String path = System.getProperty("java.class.path").split(";")[0].replace("target\\classes", "")
+                       + "src\\main\\resources\\static\\Exported-Reports";
+
+        File file = ResourceUtils.getFile(pathName + "archive-tasks.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(this.tasks);
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "Admin");
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        System.out.print(this.tasks);
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint,
+                    path + "\\archive-taskCard" + LocalDate.now() + " " + LocalDateTime.now().getHour() + " hrs "
+                            + LocalDateTime.now().getMinute() + " minutes " + ".html");
+        }
+
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path +
+                    "\\archive-taskCard" + LocalDate.now() + " " + LocalDateTime.now().getHour() + " hrs "
+                    + LocalDateTime.now().getMinute() + " minutes " + ".pdf");
+        }
+
+        if (reportFormat.equalsIgnoreCase("excel")) {
+            JRXlsxExporter exporter = new JRXlsxExporter();
+            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+
+            SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
+            configuration.setOnePagePerSheet(true);
+            configuration.setDetectCellType(true);
+            exporter.setConfiguration(configuration);
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(path +
+                    "\\archive-taskCard" + LocalDate.now() + " " + LocalDateTime.now().getHour() + " hrs "
+                    + LocalDateTime.now().getMinute() + " minutes " + ".xlsx"));
+            exporter.exportReport();
+        }
+        return "report generated in path " + path;
+    }
+
+    public void getReportArchiveTaskCard(Integer id) {
+        this.tasks= this.taskCardService.reportArchiveTaskCards(id);
+    }
+
+}
